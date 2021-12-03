1d8087cb493981c0d1446b1978a2d6ef65c61171
Donna <donnajun1214@gmail.com>
2021-12-03 10:34:31 +0900

    more added

--- src/components/Frame.vue
+++ src/components/Frame.vue
@@ -1,130 +1,121 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="cont" :style="{ display: !isMobile ? 'flex' : 'block' }">
+    <hooper
+      class="imgSlide"
+      :style="{
+        width: !isMobile ? (mode == 'vertical' ? '277px' : '60%') : '100%',
+        'margin-right': !isMobile ? '24px' : '0',
+      }"
+      :infiniteScroll="true"
+      :wheelControl="false"
+    >
+      <slide v-for="img in imgs" :key="img"
+        ><img
+          :src="require(`@/assets/${img}`)"
+          alt="img"
+          style="display: block; width: 100%; object-fit: contain"
+      /></slide>
+      <hooper-navigation slot="hooper-addons"></hooper-navigation>
+      <HooperPagination slot="hooper-addons"></HooperPagination>
+    </hooper>
+    <div style="text-align: left">
+      <div class="flexRow">
+        <div style="width: 60px; height: 60px" class="center">
+          <img
+            :src="require(`@/assets/${img}`)"
+            alt="img"
+            style="width: 100%; object-fit: contain"
+          />
+        </div>
+        <h3 style="margin-left: 8px">
+          {{ title }}
+        </h3>
+      </div>
+      <h5>{{ period }}</h5>
+      <a :href="url" target="_blank">{{ url }}</a>
+      <h4>주요 기술 스택</h4>
+      <ul>
+        <li>details</li>
+        <li>details</li>
+        <li>details</li>
+        <li>details</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import {
+  Hooper,
+  Slide,
+  Navigation as HooperNavigation,
+  Pagination as HooperPagination,
+} from "hooper";
+import "hooper/dist/hooper.css";
+
 export default {
-  name: "HelloWorld",
+  name: "Frame",
+  components: {
+    Hooper,
+    Slide,
+    HooperNavigation,
+    HooperPagination,
+  },
   props: {
-    msg: String,
+    mode: {
+      type: String,
+      require: true,
+    },
+    isMobile: {
+      type: Boolean,
+      require: true,
+    },
+    vertical: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      require: true,
+    },
+    period: {
+      type: String,
+      require: true,
+    },
+    url: {
+      type: String,
+      require: true,
+    },
+    img: {
+      type: String,
+      require: true,
+    },
+    imgs: {
+      type: Array,
+      require: true,
+    },
+    // details: {
+    //   type: Array,
+    //   require: true,
+    // },
+  },
+  computed: {
+    howToAlign() {
+      return this.isMobile && this.mode !== "vertical"
+        ? null
+        : "display:flex;flex-flow: row wrap;";
+    },
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.cont {
+  flex-flow: row wrap;
+  justify-content: start;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.imgSlide {
+  border: 1px solid gray;
+  border-radius: 16px;
+  overflow: hidden;
 }
 </style>
